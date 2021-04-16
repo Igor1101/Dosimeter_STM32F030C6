2b66a741d6c2acedad3bbe7d0c061bd9e6ecbdea
Igor1101 <igor.muravyov.2020@ukr.net>
2021-04-16 18:38:55 +0300

    conf file items upgrade

--- generic/conf.h
+++ generic/conf.h
@@ -10,9 +10,18 @@
 
 // this is sample conf file
 // only example(invalid) values are provided in sample conf file
-#define CONF_MOBILE_PHONE "+380000000000"
-#define CONF_SERV0_ADDR "901.202.303.404"
+// all values should be present as strings, they will be saved to the
+// end page of FLASH memory of the MCU
+// MOBILE PHONE FOR DIAGNOSTICS
+#define CONF_MOBILE_PHONE "+380950000000"
+// MAIN SERVER TRANSFER JSON DATA TO
+#define CONF_SERV0_ADDR "00.00.100.002"
+// SPARE SERVER
 #define CONF_SERV1_ADDR "1.3.3.4"
-#define CONF_PORT "41"
+// PORT OF SERVER APPLICATION
+#define CONF_PORT		"41"
+// DEVICE IDENTIFIER NAME STRING (30 CHARACTERS MAX)
+#define CONF_DEVICE_ID	"device_name01"
+
 
 #endif /* CONF_H_ */
